dcb12583fc1f2eeacaa7438b61c585ac415a880b
Dogac <ndakyildiz@gmail.com>
2019-07-01 00:49:38 +0300

    Project committed.

---
+++ WebApp2/WebApp2/Models/Engine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp2.Models
+{
+    public class Engine
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ WebApp2/WebApp2/wwwroot/js/ajax-functions.js
@@ -0,0 +1,53 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function getPage(action) {
+    var url = "Engines/" + action;
+
+    getRequest(url);
+}
+
+function getPage(action, _id) {
+    var url = "Engines/" + action + "/" + _id;
+
+    getRequestwithId(url, _id);
+}
+
+function getPagewithController(controller, action) {
+    var url = "/" + controller + "/" + action;
+
+    getRequest(url);
+}
+
+function getRequest(url) {
+    $.get(url, function (response) {
+        loadPage(response);
+    });
+}
+
+function getRequestwithId(url, _id) {
+    $.get(url, { id: _id }, function (response) {
+        loadPage(response);
+    });
+}
+
+function loadPage(response) {
+    var node = getParsedHtml(response);
+    var container = getContainer(node);
+
+    setContainer(container);
+}
+
+function getParsedHtml(html) {
+    return $.parseHTML(html);
+}
+
+function getContainer(node) {
+    return $(node).filter('#app-container');
+}
+
+function setContainer(text) {
+    $('#app-container').html(text);
+}
